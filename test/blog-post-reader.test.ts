1f75d3258705c5f56caf48418984d667b876949f
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-06-25 01:32:29 -0300

    Initial commit

---
+++ test/blog-post-reader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogPostReader from '../lib/blog-post-reader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-post-reader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogPostReader.BlogPostReaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
